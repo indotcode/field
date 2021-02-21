5b23d5ffaadc160eb46957e0d1d9ca7a51d96da9
Alex <tinarskiy@bk.ru>
2021-02-21 21:39:49 +0300

    Start

---
+++ src/FieldServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Indotcode\Field;
+
+use Illuminate\Support\ServiceProvider;
+
+class FieldServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
